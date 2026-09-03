b5a9e4b614f945bba0447f677785a99f72495562
shydaringdeer <shydaringdeer@users.noreply.github.com>
2026-09-02 19:22:57 -0500

    SB 2

--- src/state/supabase.js
+++ src/state/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Retrieve environment variables injected by Vite
-const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
+// Retrieve environment variables injected by Vite or Vercel
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL || import.meta.env.NEXT_PUBLIC_SUPABASE_URL || import.meta.env.SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY || import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || import.meta.env.SUPABASE_ANON_KEY;
 
 // Warn if they are missing
 if (!supabaseUrl || !supabaseAnonKey) {
